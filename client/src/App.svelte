8a92665346a7d98675da9177d8a1e70964d9be56
Cláudio Pereira <git@claudiop.com>
2022-08-18 05:20:43 +0100

    Clean App component, PT paths

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,10 +1,11 @@
 <script>
   import { onMount, tick } from "svelte";
   import Issues from "./lib/Issues.svelte";
-  import RouteViewer from "./lib/Operators.svelte";
+  import Operators from "./lib/Operators.svelte";
   import GlobalMap from "./lib/Map.svelte";
   import About from "./lib/About.svelte";
   import Home from "./lib/Home.svelte";
+  import FAQ from "./lib/FAQ.svelte";
   import Simulator from "./lib/Simulator.svelte";
   import { initCache } from "./cache.js";
   import { writable } from "svelte/store";
@@ -13,19 +14,21 @@
   let path;
 
   const pages = {
-    issues: Issues,
-    map: GlobalMap,
-    routes: RouteViewer,
+    avisos: Issues,
+    rede: GlobalMap,
+    servicos: Operators,
     simulator: Simulator,
-    about: About,
+    sobre: About,
+    faq: FAQ,
   };
 
   let links = [
     ["", "Início"],
-    ["map", "Rede"],
-    ["routes", "Serviços"],
-    ["issues", "Avisos"],
-    ["about", "Sobre nós"],
+    ["rede", "Rede"],
+    ["servicos", "Serviços"],
+    ["avisos", "Avisos"],
+    ["faq", "Questões"],
+    ["sobre", "Sobre"],
   ];
 
   async function hashchange() {
@@ -70,49 +73,25 @@
 
 <svelte:window on:hashchange={hashchange} />
 
-<div class="w-[min(920px,100%)] mx-auto z-[3000] p-2 xl:pt-4">
+<div class="w-[min(920px,100%)] mx-auto z-[3000] p-2 pb-4 xl:pt-4">
   <div class="navbar bg-base-100 shadow-xl rounded-xl">
     <div class="navbar-start">
       <div class="">
         <span class="btn btn-ghost lg:hidden" on:click={() => toggleDD(0)}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h8m-8 6h16"
-            />
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
           </svg>
         </span>
         <ul
           class="menu menu-compact mt-3 p-2 dropdown-content shadow bg-base-100 rounded-box w-52 duration-200 transition-all {$ddClasses}"
-          on:click={() => toggleDD(150)}
-        >
+          on:click={() => toggleDD(150)}>
           {#each links as k}
-            <li>
-              <a
-                class={k[0] === path ? "bg-primary text-primary-content" : ""}
-                href="#/{k[0]}">{k[1]}</a
-              >
-            </li>
+            <li><a class={k[0] === path ? "bg-primary text-primary-content" : ""} href="#/{k[0]}">{k[1]}</a></li>
           {/each}
         </ul>
       </div>
-      <a
-        class="btn btn-ghost normal-case text-xl md:text-3xl text-primary"
-        href="/"
-      >
-        <img
-          src="/logo-blue.o.svg"
-          class="w-8 pr-1"
-          alt="Início,Logo Intermodalis"
-        />
+      <a class="btn btn-ghost normal-case text-xl md:text-3xl text-primary" href="/">
+        <img src="/logo-blue.o.svg" class="w-8 pr-1" alt="Início,Logo Intermodal" />
         <span class="text-[#59befe]">inter</span>
         <span class="text-[#006d99]">modal</span>
       </a>
@@ -120,11 +99,7 @@
     <div class="navbar-end hidden lg:flex">
       <ul class="menu menu-horizontal p-0">
         {#each links as k}
-          <li>
-            <a class={k[0] === path ? "bg-base-300" : ""} href="#/{k[0]}"
-              >{k[1]}</a
-            >
-          </li>
+          <li><a class={k[0] === path ? "bg-base-300" : ""} href="#/{k[0]}">{k[1]}</a></li>
         {/each}
       </ul>
     </div>
