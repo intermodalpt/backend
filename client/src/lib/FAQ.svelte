a85b96d92085fde77c22318a3ed14ff6ac27ccd0
Cláudio Pereira <git@claudiop.com>
2022-08-18 05:20:11 +0100

    Update FAQ with collapsibles

--- client/src/lib/FAQ.svelte
+++ client/src/lib/FAQ.svelte
@@ -2,81 +2,114 @@
   <div class="card bg-base-100 shadow-xl">
     <div class="card-body">
       <h2 class="card-title">Sobre os transportes</h2>
-      <h3 class="text-lg">Representam a Carris Metropolitana?</h3>
-      <p>
-        Não temos qualquer afiliação à marca <b>Carris Metropolitana</b>. A nossa plafatorma é
-        desenvolvida por voluntários sem qualquer vínculo à TML.<br/>
-        A página oficial da Carris Metropolitana encontra-se em
-        <a class="link link-primary" href="https://www.carrismetropolitana.pt/">carrismetropolitana.pt</a>.
-      </p>
-      <h3 class="text-lg">
-        O meu transporte não apareceu/atrasou-se/fez um trajeto errado/comeu-me
-        o queijo do frigorifico.
-      </h3>
-      <p>
-        As novas operações vem seguidas de uma fase transitória que naturalmente
-        é dada a complexidades. Algumas destas complexidades são naturais. Perfeito é inimigo do bom.
-        Se tem uma queixa
-        fundamentada utilize o
-        <a class="link link-primary" href="https://www.livroreclamacoes.pt/">livro de reclamações</a>.
-        Não reclame com motoristas ou bilheteiras, não reclame nas redes
-        sociais, não utilize plataformas de terceiros, utilize
-        <b>só e apenas o livro de reclamações</b>.
-        Procure produzir <b>criticas construtivas</b> e não indignação em massa.
-      </p>
-      <p>
-        Queixas no livro de reclamações imputam dever legal à entidade de
-        produzir uma justificação. Volumes elevados de queixas levam o regulador
-        a tomar medidas.
-      </p>
+      <div tabindex="0" class="collapse border border-base-300 bg-base-100 rounded-box">
+        <div class="collapse-title text-xl font-medium">Representam a Carris Metropolitana?</div>
+        <div class="collapse-content">
+          <p>
+            Não temos qualquer afiliação à marca <b>Carris Metropolitana</b>. A nossa plafatorma é
+            desenvolvida por voluntários sem qualquer vínculo à TML.<br/>
+            A página oficial da Carris Metropolitana encontra-se em
+            <a class="link link-primary" href="https://www.carrismetropolitana.pt/">carrismetropolitana.pt</a>.
+          </p>
+        </div>
+      </div>
+      <div tabindex="0" class="collapse border border-base-300 bg-base-100 rounded-box">
+        <div class="collapse-title text-xl font-medium">Porquê é que vos falta X na Carris Metropolitana?</div>
+        <div class="collapse-content">
+          <p>
+            Porque os dados na fonte estão errados e os estamos a corrigir manualmente.
+            Infelizmente nem a ALSA nem a TML(empresa que detem a Carris Metropolitana) responderam às tentativas de contacto a fim de obtermos esclarecimentos.
+            Assim que nos forneçam dados fidedignos trataremos de os apresentar.
+          </p>
+        </div>
+      </div>
+      <div tabindex="0" class="collapse border border-base-300 bg-base-100 rounded-box">
+        <div class="collapse-title text-xl font-medium">
+          O meu transporte não apareceu/atrasou-se/fez um trajeto errado/comeu-me
+          o queijo do frigorifico.</div>
+        <div class="collapse-content">
+          <p>
+            As novas operações vem seguidas de uma fase transitória que naturalmente
+            é dada a complexidades. Algumas destas complexidades são naturais. Perfeito é inimigo do bom.
+            Se tem uma queixa
+            fundamentada utilize o
+            <a class="link link-primary" href="https://www.livroreclamacoes.pt/">livro de reclamações</a>.
+            Não reclame com motoristas ou bilheteiras, não reclame nas redes
+            sociais, não utilize plataformas de terceiros, utilize
+            <b>só e apenas o livro de reclamações</b>.
+            Procure produzir <b>criticas construtivas</b> e não indignação em massa.
+          </p>
+          <p>
+            Queixas no livro de reclamações imputam dever legal à entidade de
+            produzir uma justificação. Volumes elevados de queixas levam o regulador
+            a tomar medidas.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
   <div class="card bg-base-100 shadow-xl">
     <div class="card-body">
       <h2 class="card-title">Sobre o projeto</h2>
-      <h3 class="text-lg">
-        Quando é que estão a planear acabar o trabalho atual e adicionar novas empresas?
-      </h3>
-      <p>
-        Temos um plano definido de forma muito liberal.
-      </p>
-      <ul class="list-disc pl-4">
-        <li>Durante o mês de <b>Agosto</b> queremos finalizar a indexação da Carris Metropolitana.</li>
-        <li>Em <b>Setembro</b> queremos adicionar os TCB, a TTSL, a Fertagus, o ML, a CP e o MTS.</li>
-        <li>Em <b>Outubro</b> queremos adicionar a Carris e a MobiCascais.</li>
-        <li>Em <b>Novembro</b> queremos tornar a plataforma acessivel a pessoas portadoras de deficiências físicas.</li>
-        <li>Em <b>Janeiro</b> queremos estar prontos para o lançamento da Carris Metropolitana na margem norte do Tejo.</li>
-      </ul>
-      <h3 class="text-lg">Posso ajudar?</h3>
-      <p>
-        Claro que sim! Tudo o que contribuires a projetos nos quais este se baseie, ajuda-nos indiretamente.
-        Também nos podes enviar fotos de infraestruturas (como paragens) à media que passas por elas. Tudo é indexado e
-        publicado.
-      </p>
-      <ul class="list-disc pl-4">
-        <li>Aplicações como o <a class="link link-primary" href="https://wiki.openstreetmap.org/wiki/StreetComplete">StreetComplete</a>
-          são uma boa forma de melhorar o mapa com pouco esforço.</li>
-        <li>Tira fotos a infraestruturas (como paragens) à medida que passares por elas. Tudo é indexado e
-          publicado enquanto dados abertos.</li>
-        <li>Individuos com mais mestria dos computadores podem contribuir conteudos artisticos ou programáticos; mas entra
-          em contacto connosco antes disso.</li>
-        <li>Divulga-nos. Vota. Faz a tua voz ouvida dizendo que queres boas infraestruturas públicas. Contribui para o urbanismo da tua cidade.</li>
-      </ul>
-      <p>
-        Não estamos a aceitar doações monetárias, mas projetos como o
-        <a class="link link-primary" href="https://lisboaparapessoas.pt/contribuir/">Lisboa Para Pessoas</a> fazem um trabalho imenso; ajuda-os.
-      </p>
-      <h3 class="text-lg">Como é que financiam o projeto?</h3>
-      <p>
-        Com o nosso próprio dinheiro, pro bono. Não existe qualquer
-        financiamento externo, seja por parte de empresas de transportes, do
-        governo ou de qualquer outra entidade privada.
-      </p>
-      <h3 class="text-lg">Qual a vossa politica de privacidade?</h3>
-      <p>
-        Não usamos cookies nem qualquer outro estratagema para extorquir dados dos visitantes.<br>
-        Navegue à vontade que não está a ser observado.
-      </p>
+      <div tabindex="0" class="collapse border border-base-300 bg-base-100 rounded-box">
+        <div class="collapse-title text-xl font-medium">
+          Quando é que estão a planear acabar o trabalho atual e adicionar novas empresas?
+        </div>
+        <div class="collapse-content">
+          <p>
+            Temos um plano definido de forma muito liberal.
+          </p>
+          <ul class="list-disc pl-4">
+            <li>Durante o mês de <b>Agosto</b> queremos finalizar a indexação da Carris Metropolitana.</li>
+            <li>Em <b>Setembro</b> queremos adicionar os TCB, a TTSL, a Fertagus, o ML, a CP e o MTS.</li>
+            <li>Em <b>Outubro</b> queremos adicionar a Carris e a MobiCascais.</li>
+            <li>Em <b>Novembro</b> queremos tornar a plataforma acessivel a pessoas portadoras de deficiências físicas.</li>
+            <li>Em <b>Janeiro</b> queremos estar prontos para o lançamento da Carris Metropolitana na margem norte do Tejo.</li>
+          </ul>
+        </div>
+      </div>
+      <div tabindex="0" class="collapse border border-base-300 bg-base-100 rounded-box">
+        <div class="collapse-title text-xl font-medium">Posso ajudar?</div>
+        <div class="collapse-content">
+          <p>
+            Claro que sim! Tudo o que contribuires a projetos nos quais este se baseie, ajuda-nos indiretamente.
+            Também nos podes enviar fotos de infraestruturas (como paragens) à media que passas por elas. Tudo é indexado e
+            publicado.
+          </p>
+          <ul class="list-disc pl-4">
+            <li>Aplicações como o <a class="link link-primary" href="https://wiki.openstreetmap.org/wiki/StreetComplete">StreetComplete</a>
+              são uma boa forma de melhorar o mapa com pouco esforço.</li>
+            <li>Tira fotos a infraestruturas (como paragens) à medida que passares por elas. Tudo é indexado e
+              publicado enquanto dados abertos.</li>
+            <li>Individuos com mais mestria dos computadores podem contribuir conteudos artisticos ou programáticos; mas entra
+              em contacto connosco antes disso.</li>
+            <li>Divulga-nos. Vota. Faz a tua voz ouvida dizendo que queres boas infraestruturas públicas. Contribui para o urbanismo da tua cidade.</li>
+          </ul>
+          <p>
+            Não estamos a aceitar doações monetárias, mas projetos como o
+            <a class="link link-primary" href="https://lisboaparapessoas.pt/contribuir/">Lisboa Para Pessoas</a> fazem um trabalho imenso; ajuda-os.
+          </p>
+        </div>
+      </div>
+      <div tabindex="0" class="collapse border border-base-300 bg-base-100 rounded-box">
+        <div class="collapse-title text-xl font-medium">Como é que financiam o projeto?</div>
+        <div class="collapse-content">
+          <p>
+            Com o nosso próprio dinheiro, pro bono. Não existe qualquer
+            financiamento externo, seja por parte de empresas de transportes, do
+            governo ou de qualquer outra entidade privada.
+          </p>
+        </div>
+      </div>
+      <div tabindex="0" class="collapse border border-base-300 bg-base-100 rounded-box">
+        <div class="collapse-title text-xl font-medium">Qual a vossa politica de privacidade?</div>
+        <div class="collapse-content">
+          <p>
+            Não usamos cookies nem qualquer outro estratagema para extorquir dados dos visitantes.<br>
+            Navegue à vontade que não está a ser observado.
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </div>
