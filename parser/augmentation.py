36cea23c9b20bf4a16a20e907b314eb11568abfa
Cláudio Pereira <git@claudiop.com>
2022-08-26 02:20:13 +0100

    Update OSM importer script

--- parser/augmentation.py
+++ parser/augmentation.py
@@ -10,50 +10,76 @@ from bs4 import BeautifulSoup
 from consts import MUN_AML, DB_PATH
 
 
-def update_osm_stops(district="Setúbal"):
-    osm_query = \
-        f"""
-        area
-          ["name"="{district}"]
-          ["boundary"="administrative"];
-        (
-          node
-            ["highway"="bus_stop"]
-            (area);
-        );
-        out body;
-        """
-
-    osm_query_url = "https://overpass-api.de/api/interpreter?data=" + quote_plus(osm_query)
-
-    bus_stops_resp = requests.get(osm_query_url)
-    soup = BeautifulSoup(bus_stops_resp.text, 'xml')
-
+def update_osm_stops(districts=("Setúbal", "Lisboa")):
     nodes = []
-    for node in soup.findAll('node'):
-        attrs = node.attrs.copy()
-        attrs['lat'] = float(attrs['lat'])
-        attrs['lon'] = float(attrs['lon'])
-        for child in node.findAll('tag'):
-            key = child.attrs['k']
-            if key in (
-                    'highway', 'network:wikidata', 'departures_board', 'lit', 'bin', 'bus', 'mapillary',
-                    'network:wikipedia',
-                    'survey:date', 'public_transport'):
-                continue
 
-            attrs[key] = child.attrs['v']
+    def collect_district_nodes(district):
+        osm_query = \
+            f"""
+            area
+              ["name"="{district}"]
+              ["boundary"="administrative"];
+            (
+              node
+                ["highway"="bus_stop"]
+                (area);
+            );
+            out body;
+            """
 
-        nodes.append(attrs)
+        osm_query_url = "https://overpass-api.de/api/interpreter?data=" + quote_plus(osm_query)
+
+        bus_stops_resp = requests.get(osm_query_url)
+        soup = BeautifulSoup(bus_stops_resp.text, 'xml')
+
+        for node in soup.findAll('node'):
+            attrs = node.attrs.copy()
+            attrs['lat'] = float(attrs['lat'])
+            attrs['lon'] = float(attrs['lon'])
+            for child in node.findAll('tag'):
+                key = child.attrs['k']
+                if key in (
+                        'highway', 'network:wikidata', 'departures_board', 'lit', 'bin', 'bus', 'mapillary',
+                        'network:wikipedia',
+                        'survey:date', 'public_transport'):
+                    continue
+
+                attrs[key] = child.attrs['v']
+
+            nodes.append(attrs)
+
+    for district in districts:
+        sleep(5)
+        collect_district_nodes(district)
+
+    upstream_ids = {node['id'] for node in nodes}
 
     conn = sqlite3.connect(DB_PATH)
     cur = conn.cursor()
+
+    res = cur.execute(
+        "SELECT osm_name, lat, lon, external_id FROM Stops "
+        "WHERE source='osm' AND external_id NOT IN "
+        f"({','.join(['?'] * len(upstream_ids))});",
+        (list(upstream_ids)))
+
+    updated = 0
+    inserted = 0
+    deleted = 0
+
+    for row in res.fetchall():
+        name, lat, lon, external_id = row
+        deleted += 1
+        print(f"{external_id} - {name} ({lon};{lat}) has been deleted. Nuke?")
+        ans = input()
+        if ans.lower().strip() == 'y':
+            res = cur.execute("DELETE FROM Stops WHERE source='osm' AND external_id=?", (external_id,))
 
     for node in nodes:
         osm_id, name, lat, lon = node['id'], node.get('name', None), node['lat'], node['lon']
 
         res = cur.execute(
-            "SELECT name, lat, lon FROM Stops WHERE source='osm' AND external_id = ?;",
+            "SELECT osm_name, lat, lon FROM Stops WHERE source='osm' AND external_id = ?;",
             (osm_id,))
 
         existing = res.fetchone()
@@ -62,15 +88,17 @@ def update_osm_stops(district="Setúbal"):
             existing_name, existing_lat, existing_lon = existing
             if existing_name == name and existing_lat == lat and existing_lon == lon:
                 continue
+            updated += 1
             print(f"Updated: ({osm_id}){name} ({lon} {lat})")
             res = cur.execute(
-                "UPDATE Stops SET name=?, lat=?, lon=? WHERE source='osm' AND external_id = ?;",
+                "UPDATE Stops SET osm_name=?, lat=?, lon=? WHERE source='osm' AND external_id = ?;",
                 (name, lat, lon, osm_id))
 
         else:
+            inserted += 1
             print(f"Inserted: ({osm_id}){name} ({lon} {lat})")
             res = cur.execute(
-                "INSERT INTO Stops(name, source, lat, lon, external_id) "
+                "INSERT INTO Stops(osm_name, source, lat, lon, external_id) "
                 "VALUES (?, 'osm', ?, ?,?) "
                 "ON CONFLICT DO NOTHING;",
                 (name, lat, lon, osm_id))
