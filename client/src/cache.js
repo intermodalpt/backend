e9ba5e89f3d6e041ba0fa380ff701b68c3e62238
Cláudio Pereira <git@claudiop.com>
2022-10-03 20:29:21 +0100

    Client: Overhaul operator listing

--- client/src/cache.js
+++ client/src/cache.js
@@ -7,7 +7,17 @@ export const routes = writable([]);
 export const routeDict = derived(routes, ($routes) => {
   return Object.fromEntries($routes.map(route => [route.id, route]));
 });
-
+export const operators = writable({
+  1: {id: 1, name: "Carris Metropolitana", tag: "cmet"},
+  2: {id: 2, name: "Transportes Colectivos do Barreiro", tag: "tcb"},
+  3: {id: 3, name: "Carris", tag: "carris"},
+  4: {id: 4, name: "MobiCascais", tag: "mobic"},
+  5: {id: 5, name: "Comboios de Portugal", tag: "cp"},
+  6: {id: 6, name: "Fertagus", tag: "fert"},
+  7: {id: 7, name: "Metro Transportes do Sul", tag: "mts"},
+  8: {id: 8, name: "Metro de Lisboa", tag: "ml"},
+  9: {id: 9, name: "Transtejo e Soflusa", tag: "ttsl"}
+});
 
 export async function initCache() {
   routes.set(await fetch(`${api_server}/api/routes`).then(r => r.json()));
