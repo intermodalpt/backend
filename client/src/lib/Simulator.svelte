51f8be1fc4006beac7a0c0c97310caa3d5ec167b
Cláudio Pereira <git@claudiop.com>
2022-12-19 02:47:27 +0000

    Drop unrelated sub projects

--- client/src/lib/Simulator.svelte
+++
@@ -1,5 +0,0 @@
-<h1>Simulador</h1>
-<div class="content padded">
-  <h1>...</h1>
-</div>
-
--- client/src/settings.js
+++
@@ -1,5 +0,0 @@
-import {writable} from "svelte/store";
-
-export const api_server = "https://api.intermodal.pt"
-
-export const mode = writable(localStorage.getItem("mode"));
