f0847e7f981810590e252b544fde3e67941bd6fd
Cláudio Pereira <git@claudiop.com>
2023-08-14 00:44:23 +0100

    Tool to update the stop parishes

---
+++ tools/src/update_parishes/main.rs
@@ -0,0 +1,160 @@
+/*
+    Intermodal, transportation information aggregator
+    Copyright (C) 2023  Cláudio Pereira
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+mod sql;
+
+use std::sync::Mutex;
+
+use config::Config;
+use geo::{Contains, LineString, MultiPolygon, Polygon};
+use rayon::prelude::*;
+use sqlx::postgres::PgPool;
+
+use commons::models::geo::{GeoJson, GeoJsonGeometry, Parish};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+#[tokio::main]
+async fn main() {
+    let settings = Config::builder()
+        .add_source(config::File::with_name("./settings.toml"))
+        .add_source(config::Environment::with_prefix("SETTINGS"))
+        .build()
+        .unwrap();
+
+    let pool = PgPool::connect(&settings.get_string("db").expect("db not set"))
+        .await
+        .expect("Unable to connect to the database");
+
+    match update_parishes(&pool).await {
+        Ok(_) => {
+            println!("Done");
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
+        }
+    }
+}
+
+async fn update_parishes(pool: &PgPool) -> Result<()> {
+    let parishes = sql::fetch_parishes(pool).await?;
+
+    let polygons = parishes
+        .into_iter()
+        .map(|p| {
+            let geojson: GeoJson = serde_json::from_value(p.geojson)
+                .map_err(|err| {
+                    eprintln!("Error parsing parish {}: {}", p.id, err);
+                })
+                .unwrap();
+
+            let multipoly = match geojson.geometry {
+                GeoJsonGeometry::Polygon { coordinates } => {
+                    MultiPolygon::from(vec![poly_from_coords(coordinates)])
+                }
+                GeoJsonGeometry::MultiPolygon { coordinates } => {
+                    MultiPolygon::from(
+                        coordinates
+                            .into_iter()
+                            .map(poly_from_coords)
+                            .collect::<Vec<_>>(),
+                    )
+                }
+            };
+
+            (p.id, p.name, multipoly)
+        })
+        .collect::<Vec<_>>();
+
+    let stops = sql::fetch_stops(pool, false).await?;
+
+    let stop_parish_pairs = Mutex::new(vec![]);
+
+    stops.par_iter().for_each(|stop| {
+        if stop.parish.is_some() {
+            return;
+        }
+        let point = geo::Point::new(stop.lon.unwrap(), stop.lat.unwrap());
+        for (id, name, multipoly) in &polygons {
+            if multipoly.contains(&point) {
+                stop_parish_pairs.lock().unwrap().push((stop.id, *id));
+                println!(
+                    "Stop {} ({}) is in parish {}",
+                    stop.name.as_ref().unwrap_or(
+                        stop.official_name.as_ref().unwrap_or(
+                            stop.osm_name.as_ref().unwrap_or(&"?".to_string())
+                        )
+                    ),
+                    stop.id,
+                    name
+                );
+                break;
+            }
+        }
+    });
+
+    // for (stop_id, parish_id) in stop_parish_pairs.into_inner().unwrap() {
+    //     sql::update_stop_parish(&pool, stop_id, parish_id).await?;
+    // }
+
+    for stop in stops {
+        if stop.parish.is_some() {
+            continue;
+        }
+        let point = geo::Point::new(stop.lon.unwrap(), stop.lat.unwrap());
+        for (id, name, multipoly) in &polygons {
+            if multipoly.contains(&point) {
+                sql::update_stop_parish(&pool, stop.id, *id).await?;
+                println!(
+                    "Stop {} ({}) is in parish {}",
+                    stop.name.unwrap_or(
+                        stop.official_name.unwrap_or(
+                            stop.osm_name.unwrap_or("?".to_string())
+                        )
+                    ),
+                    stop.id,
+                    name
+                );
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn poly_from_coords(coordinates: Vec<Vec<Vec<f64>>>) -> Polygon {
+    let mut polygons = coordinates.into_iter();
+    let outer_coords = polygons.next().unwrap();
+    let outer_line = LineString::from(
+        outer_coords
+            .into_iter()
+            .map(|p| (p[0], p[1]))
+            .collect::<Vec<_>>(),
+    );
+
+    let inner_lines = polygons
+        .map(|p| {
+            LineString::from(
+                p.into_iter().map(|p| (p[0], p[1])).collect::<Vec<_>>(),
+            )
+        })
+        .collect::<Vec<_>>();
+
+    Polygon::new(outer_line, inner_lines)
+}
---
+++ tools/src/update_parishes/sql.rs
@@ -0,0 +1,100 @@
+/*
+    Intermodal, transportation information aggregator
+    Copyright (C) 2023  Cláudio Pereira
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+use sqlx::postgres::PgPool;
+
+use commons::models::geo;
+use commons::models::stops;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+pub(crate) async fn fetch_stops(
+    pool: &PgPool,
+    filter_used: bool,
+) -> Result<Vec<stops::Stop>> {
+    sqlx::query!(
+"SELECT id, source, name, official_name, osm_name, short_name, locality, street,
+    door, lat, lon, external_id, notes, updater, update_date,
+    parish, tags, accessibility_meta, refs,
+    verification_level, service_check_date, infrastructure_check_date
+FROM stops")
+    .fetch_all(pool)
+    .await?
+    .into_iter()
+    .map(|r| {
+        Ok(stops::Stop {
+            id: r.id,
+            source: r.source,
+            name: r.name,
+            official_name: r.official_name,
+            osm_name: r.osm_name,
+            short_name: r.short_name,
+            locality: r.locality,
+            street: r.street,
+            door: r.door,
+            lat: r.lat,
+            lon: r.lon,
+            external_id: r.external_id,
+            refs: r.refs,
+            notes: r.notes,
+            updater: r.updater,
+            update_date: r.update_date,
+            parish: r.parish,
+            tags: r.tags,
+            a11y: serde_json::from_value(r.accessibility_meta)?,
+            verification_level: r.verification_level as u8,
+            service_check_date: r.service_check_date,
+            infrastructure_check_date: r.infrastructure_check_date,
+        })
+    })
+    .collect()
+}
+
+pub(crate) async fn fetch_parishes(pool: &PgPool) -> Result<Vec<geo::Parish>> {
+    Ok(sqlx::query_as!(
+        geo::Parish,
+        r#"
+SELECT parishes.id, parishes.name, parishes.short_name, municipalities.name as municipality,
+    municipalities.zone, parishes.polygon, parishes.geojson
+FROM parishes
+JOIN municipalities ON parishes.municipality = municipalities.id
+    "#
+    )
+        .fetch_all(pool)
+        .await?)
+}
+
+pub(crate) async fn update_stop_parish(
+    pool: &PgPool,
+    stop_id: i32,
+    parish: i32,
+) -> Result<()> {
+    sqlx::query!(
+        r#"
+UPDATE stops
+SET parish = $1
+WHERE id = $2
+        "#,
+        parish,
+        stop_id
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
+}
