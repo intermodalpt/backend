6929cbc4bb3f38ec63aae76145a5104107500ddb
Cláudio Pereira <git@claudiop.com>
2023-11-07 02:15:03 +0000

    Add a lib.rs to allow for integration testing

---
+++ api_server/src/lib.rs
@@ -0,0 +1,35 @@
+/*
+    Intermodal, transportation information aggregator
+    Copyright (C) 2023  Cláudio Pereira
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#![allow(unused)]
+pub mod auth;
+pub mod contrib;
+pub mod errors;
+pub mod geo;
+pub mod gtfs;
+pub mod http;
+pub mod misc;
+pub mod operators;
+pub mod pics;
+pub mod routes;
+pub mod state;
+pub mod stops;
+pub mod utils;
+
+pub use errors::Error;
+pub use state::{AppState, Cached, State};
